58a4563475808686e9078c184c6766822c1e2d42
Cho-yunji <favorcho@hanmail.net>
2021-06-22 00:16:36 +0900

    끝말잇기 완성

---
+++ jQuery_09_Advanced_slideshow_BASE/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+var slideshow = $('.slideshow');
+var slides = slideshow.find('.slides');
+var slide = slides.find('a');
+var slideWidth = slides.find('img').width();
+
+console.log(slideWidth); 
+
+
+});
